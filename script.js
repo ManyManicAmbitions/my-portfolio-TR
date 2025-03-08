f99364af0a5787c9eb2f9b841569cc9249be99fa
ManyManicAmbitions <russelltyrel9@gmail.com>
2025-03-07 19:14:24 -0500

    Added script for contact form

--- script.js
+++ script.js
@@ -11,4 +11,33 @@ buttons.forEach(button => {
             }
     });
 } );
-    
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    let errorMessage = "";
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+        if (!name) {
+            errorMessage += "Name mandatory";
+        }
+        if (!email) {
+            errorMessage += "Email mandatory";
+        } else if (!validateEmail(email)) {
+            errorMessage += "Email invalid";
+        }
+        if (!phone) {
+            errorMessage += "Phone number mandatory";
+        }
+        if (!message) {
+            errorMessage += "Message field mandatory";
+        }
+        if (errorMessage) {
+            event.preventDefault();
+            document.getElementById("error-message").innerText = errorMessage;
+        }
+});
+
+function validateEmail(email) {
+ return email.indexOf("@") > -1 && email.indexOf(".") > -1;  
+} 
